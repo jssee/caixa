408d7a7961dd904c580ef72c53a5227a07529698
Jesse Hoyos <jesse@mojotech.com>
2020-07-17 01:07:48 -0400

    fix: remove unused props in box.stories

--- stories/Box.stories.tsx
+++ stories/Box.stories.tsx
@@ -11,7 +11,7 @@ const styles = {
 
 // By passing optional props to this story, you can control the props of the component when
 // you consume the story in a test.
-export const Default = (props?: Partial<Props>) => (
+export const Default = () => (
   <Box
     d="flex"
     color="red"
